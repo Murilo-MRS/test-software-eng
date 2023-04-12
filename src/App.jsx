00ab2f0d6174c733b35a8666a4d83c09daf7a347
Murilo-MRS <murilosantana1993@gmail.com>
2023-04-12 20:49:07 -0300

    chore: config projeto react+rtl+mui-RRD-eslint-javascript

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Routes from './Routes/Routes';
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <Routes />
+  );
+}
+
+export default App;
