4f396bb9dd65266c428bca74c8347a7d0388b6c8
Murilo-MRS <murilosantana1993@gmail.com>
2023-04-18 00:39:36 -0300

    fix: remoce console

--- frontend/utils/axios.js
+++ frontend/utils/axios.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-console.log(import.meta.env.VITE_ENDPOINT);
 const getRequest = async (endpoint) => {
   const { data } = await axios.get(`${import.meta.env.VITE_ENDPOINT}${endpoint}`);
   return data;
